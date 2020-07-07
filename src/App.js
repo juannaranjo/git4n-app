611d95a7ae9708b8d78420392301f03a42a7b8f8
Juan Naranjo <juandnaranjol@gmail.com>
2020-07-07 12:40:00 -0500

    feat: create CandidateInfo Component, reorder css to make it modular, add img logos, and create the cookies utils

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import CandidateInfoForm from './components/candidateInfoForm'
-import './App.css';
+import CandidateInfo from './components/candidateInfo'
+import cookieUtils from './utils/cookies'
+import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App-git4n">
-      <header className="git4n-header">
-        <div className="git4n-header-logo">
-          <img src="https://www.s4n.co/logo.svg" alt="s4n-logo"/>
-          <h2> Git4n App </h2>
-        </div>
-        <p> This app allows you to search the public github repos that a person has </p>
-      </header>
-      <main>
-        <CandidateInfoForm/>
-      </main>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      hasUserCookie: false    
+    };
+  }
+
+  componentDidMount(){
+    this.hasUserCookie()
+  }
+  
+  hasUserCookie(){
+    if(cookieUtils.get()){
+      this.updateUserCookie(true);
+    }else{
+      this.updateUserCookie(false);
+    }
+  }
+
+  updateUserCookie = (state) => {
+    this.setState({hasUserCookie: state});
+  }
+
+  render(){
+    return (
+      <div className="App-git4n">
+        <header className="git4n-header">
+          <div className="git4n-header-logo">
+            <img src="/img/logo.svg" alt="s4n-logo"/>
+            <h2> Git4n App </h2>
+          </div>
+          <p> This app allows you to search the public github repos that a person has </p>
+        </header>
+        <main>
+          {this.state.hasUserCookie && <CandidateInfo />}
+          {!this.state.hasUserCookie && <CandidateInfoForm updateUserCookie={this.updateUserCookie}/>}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
